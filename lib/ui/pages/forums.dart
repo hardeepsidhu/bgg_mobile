c59d2fa73ad0a74312953cc6d1a1c0026b34ae53
Hardeep Sidhu <hardeeps@zymbiotic.com>
2019-11-23 23:05:27 -0800

    Empty forums are headers in the global forums list.

--- lib/ui/pages/forums.dart
+++ lib/ui/pages/forums.dart
@@ -12,8 +12,10 @@ import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 class ForumsArguments {
   final String title;
   final dynamic object;
+  final bool emptyForumsAreHeaders;
 
-  ForumsArguments(this.title, this.object);
+  ForumsArguments(this.title, this.object,
+      [this.emptyForumsAreHeaders = false]);
 }
 
 class Forums extends StatelessWidget {
@@ -21,9 +23,9 @@ class Forums extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    ForumsArguments args =  ModalRoute.of(context).settings.arguments;
+    ForumsArguments args = ModalRoute.of(context).settings.arguments;
     if (args == null) {
-      args = ForumsArguments('Forums', null);
+      args = ForumsArguments('Forums', null, true);
     }
 
     return new Scaffold(
@@ -31,14 +33,15 @@ class Forums extends StatelessWidget {
           title: Text(args.title),
         ),
         drawer: (args.object == null) ? AppDrawer() : null,
-        body: _ForumsWidget(args.object));
+        body: _ForumsWidget(args.object, args.emptyForumsAreHeaders));
   }
 }
 
 class _ForumsWidget extends StatefulWidget {
   final dynamic object;
+  final bool emptyForumsAreHeaders;
 
-  _ForumsWidget(this.object);
+  _ForumsWidget(this.object, this.emptyForumsAreHeaders);
 
   @override
   createState() => new _ForumsWidgetState();
@@ -50,7 +53,7 @@ class _ForumsWidgetState extends State<_ForumsWidget> {
   @override
   Widget build(BuildContext context) {
     if (_future == null) {
-      _future = Api.get().getForums(widget.object);
+      _future = _getForums(widget.object);
     }
 
     return FutureWidgetBuilder.buildListViewSeparated((context, index, obj) {
@@ -64,20 +67,42 @@ class _ForumsWidgetState extends State<_ForumsWidget> {
             );
           }
         },
-        child: Row(children:[Expanded(child:_forumCell(obj))]),
+        child: Row(children: [Expanded(child: _forumCell(obj))]),
       );
     }, _future);
   }
 
+  Future<List<Forum>> _getForums(dynamic object) async {
+    var forums = await Api.get().getForums(object);
+
+    if (!widget.emptyForumsAreHeaders) {
+      forums.removeWhere(
+          (forum) => (forum.numThreads == 0 && forum.numPosts == 0));
+    }
+
+    return forums;
+  }
+
   Widget _forumCell(Forum forum) {
+    if (widget.emptyForumsAreHeaders &&
+        forum.numThreads == 0 &&
+        forum.numPosts == 0) {
+      return Text(
+        forum.title,
+        style: UIHelper.textStyle(size: 18.0, weight: FontWeight.bold),
+        textAlign: TextAlign.left,
+      );
+    }
+
     return Padding(
       padding: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 5.0),
       child: new Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(forum.title, style: UIHelper.textStyle(weight: FontWeight.bold)),
-          HtmlWidget(forum.description,
-            textStyle:UIHelper.textStyle(size: 14.0, weight: FontWeight.w300),
+          HtmlWidget(
+            forum.description,
+            textStyle: UIHelper.textStyle(size: 14.0, weight: FontWeight.w300),
             bodyPadding: EdgeInsets.all(0),
           ),
           Padding(
